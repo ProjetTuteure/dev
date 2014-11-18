ebf629de0dea67ac9561f95d0d7701022c6e6cd2
ThibaultTricard <thibault.tricard@hotmail.fr>
2014-11-18 17:25:11 +0100

    refactoring du main

--- src/gpi/MainApp.java
+++ src/gpi/MainApp.java
@@ -15,21 +15,39 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private TabPane rootLayout;
+    private Tab SiteOverview;
+    private Tab AncienneteOverview;
+    private Tab EtatOverview;
+    private Tab AvanceOverview;
+    private Tab ParametreOverview;
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Adam SAS");
-
         initRootLayout();
-
-        showSiteOverview();
-        showAncienneteOverview();
-        showEtatOverview();
-        showAvanceOverview();
-        showParametreOverview();
+        initOverview(this.SiteOverview, "Site");
+        initOverview(this.AncienneteOverview, "Anciennete");
+        initOverview(this.EtatOverview, "Etat");
+        initOverview(this.AvanceOverview, "Avance");
+        initOverview(this.ParametreOverview, "Parametre");
     }
 
+    public void initOverview(Tab tab,String nom){
+        try {
+            tab = new Tab();
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/"+nom+"Overview.fxml"));
+            AnchorPane siteOverview = (AnchorPane) loader.load();
+            tab.setText(nom);
+            tab.setContent(siteOverview);
+            // Set person overview into the center of root layout.
+            rootLayout.getTabs().add(tab);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * Initializes the root layout.
      */
@@ -39,7 +57,6 @@ public class MainApp extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
             rootLayout = (TabPane) loader.load();
-
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -48,90 +65,7 @@ public class MainApp extends Application {
         }
     }
 
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showSiteOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/SiteOverview.fxml"));
-            AnchorPane siteOverview = (AnchorPane) loader.load();
 
-            Tab tab = new Tab();
-            tab.setText("Site");
-            tab.setContent(siteOverview);
-            // Set person overview into the center of root layout.
-            rootLayout.getTabs().add(tab);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void showAncienneteOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/AncienneteOverview.fxml"));
-            AnchorPane AncienneteOverview = (AnchorPane) loader.load();
-
-            Tab tab = new Tab();
-            tab.setText("Ancienneté");
-            tab.setContent(AncienneteOverview);
-            // Set person overview into the center of root layout.
-            rootLayout.getTabs().add(tab);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void showEtatOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/EtatOverview.fxml"));
-            AnchorPane EtatOverview = (AnchorPane) loader.load();
-
-            Tab tab = new Tab();
-            tab.setText("Etat");
-            tab.setContent(EtatOverview);
-            // Set person overview into the center of root layout.
-            rootLayout.getTabs().add(tab);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void showAvanceOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/AvanceOverview.fxml"));
-            AnchorPane AvanceOverview = (AnchorPane) loader.load();
-
-            Tab tab = new Tab();
-            tab.setText("Avancé");
-            tab.setContent(AvanceOverview);
-            // Set person overview into the center of root layout.
-            rootLayout.getTabs().add(tab);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showParametreOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/ParametreOverview.fxml"));
-            AnchorPane ParametreOverview = (AnchorPane) loader.load();
-
-            Tab tab = new Tab();
-            tab.setText("Paramètre");
-            tab.setContent(ParametreOverview);
-            // Set person overview into the center of root layout.
-            rootLayout.getTabs().add(tab);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     /**
      * Returns the main stage.
      * @return
