26b539c1fe028a9bf33af0f78700ca8919ab0bf1
Limmilkki <totor-du-40@hotmail.fr>
2014-11-24 00:30:49 +0100

    Ancienneté V3

--- src/gpi/metier/controller/ComboboxController.java
+++ src/gpi/metier/controller/ComboboxController.java
@@ -15,22 +15,27 @@ import javafx.scene.control.ComboBox;
 public class ComboboxController implements Initializable {
 
     @FXML
-    private ComboBox<String> comboboxSite;
+    private ComboBox<String> comboboxSiteAvanceOverview;
     @FXML
-    private ComboBox<String> comboboxAnciennete;
+    private ComboBox<String> comboboxAncienneteAvanceOverview;
     @FXML
-    private ComboBox<String> comboboxType;
+    private ComboBox<String> comboboxTypeAvanceOverview;
+    @FXML
+    private ComboBox<String> comboboxSiteAncienneteOverview;
+    @FXML
+    private ComboBox<String> comboboxTypeAncienneteOverview;
 
-
-    ObservableList<String> list1 = FXCollections.observableArrayList("Agen","Bordeaux","Chateroux","Guéret","Limoges","Montluçon","Saint Agan","Saint Junien");
+    ObservableList<String> list1 = FXCollections.observableArrayList("Agen","Bordeaux","Chateroux","Guéret","Limoges","Montluçon","Saint Agan","Saint Junien","Tout");
     ObservableList<String> list2 = FXCollections.observableArrayList("moins d'un ans","moins de deux ans","moins de trois ans","moins de quattre ans","moins de cinq ans","moins de six ans","moins de sept ans","plus de sept ans");
-    ObservableList<String> list3 = FXCollections.observableArrayList("Ordinateur","Switch","Routeur","Clé 3G");
+    ObservableList<String> list3 = FXCollections.observableArrayList("Ordinateur","Switch","Routeur","Clé 3G","Tout");
 
+    
     @Override
     public void initialize(URL url,ResourceBundle rb){
-        comboboxSite.setItems(list1);
-        comboboxAnciennete.setItems(list2);
-        comboboxType.setItems(list3);
-
+        comboboxSiteAvanceOverview.setItems(list1);
+        comboboxAncienneteAvanceOverview.setItems(list2);
+        comboboxTypeAvanceOverview.setItems(list3);
+        //comboboxSiteAncienneteOverview.setItems(list1);
+        //comboboxTypeAncienneteOverview.setItems(list3);
     }
 }
