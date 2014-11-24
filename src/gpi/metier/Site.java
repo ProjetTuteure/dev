9c48daee2a5fddf818d430415852b90cea002572
cedricCaffy <cedric.caffy@hotmail.fr>
2014-11-24 13:36:06 +0100

    SiteOverview

--- src/gpi/metier/Site.java
+++ src/gpi/metier/Site.java
@@ -1,41 +1,57 @@
 package gpi.metier;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * Created by thibault on 22/11/14.
  */
 public class Site {
-    private int idSite;
-    private String nomSte;
-    private String cheminImage;
+    private SimpleIntegerProperty idSite;
+    private SimpleStringProperty nomSte;
+    private SimpleStringProperty cheminImage;
 
     public String getNomSte() {
-        return nomSte;
+        return this.nomSte.get();
     }
 
+    public SimpleStringProperty getNomSteProperty()
+    {
+    	return this.nomSte;
+    }
     public void setNomSte(String nomSte) {
-        this.nomSte = nomSte;
+        this.nomSte.set(nomSte);
     }
 
-    public int getIdSite() {
+    public SimpleIntegerProperty getIdSiteProperty() {
         return idSite;
+    }
+    
+    public int getIdSite()
+    {
+    	return this.idSite.get();
     }
 
     public void setIdSite(int idSite) {
-        this.idSite = idSite;
+        this.idSite.set(idSite);
     }
 
     public String getCheminImage() {
-        return cheminImage;
+        return cheminImage.get();
+    }
+    
+    public SimpleStringProperty getCheminImageProperty()
+    {
+    	return this.cheminImage;
     }
 
     public void setCheminImage(String cheminImage) {
-        this.cheminImage = cheminImage;
+        this.cheminImage.set(cheminImage);
     }
     
     public Site(int idSite, String nomSte, String cheminImage) {
-
-        this.idSite = idSite;
-        this.nomSte = nomSte;
-        this.cheminImage = cheminImage;
+        this.idSite = new SimpleIntegerProperty(idSite);
+        this.nomSte=new SimpleStringProperty(nomSte);
+        this.cheminImage=new SimpleStringProperty(cheminImage);
     }
 }
--- src/gpi/view/SiteOverviewController.java
+++ src/gpi/view/SiteOverviewController.java
@@ -1,28 +1,83 @@
 package gpi.view;
 
+import java.awt.Panel;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import gpi.MainApp;
 import gpi.bd.Donnee;
 import gpi.metier.Site;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class SiteOverviewController {
+	@FXML
 	private GridPane gp_site;
-	private Donnee donnee;
+	
+	@FXML
+	private ImageView image_site;
+	
+	@FXML
+	private Label nomSite;
+
+	@FXML
 	private ObservableList<Site> sites;
+	
+	@FXML
+	private Donnee donnee;
+	
+	@FXML
+	private MainApp mainApp;
 	
 	public SiteOverviewController()
 	{
-		this.donnee=new Donnee();
+		/*this.donnee=new Donnee();
 		this.sites=donnee.getSiteData();
-		this.gp_site=new GridPane();
+		this.gp_site=new GridPane();*/
 	}
 	
 	public int getNbSite()
 	{
 		return this.sites.size();
+	}
+	
+	@FXML
+	private void initialize()
+	{
+		/*
+		donnee=new Donnee();
+		this.ajouterVilleGridPane(donnee.getSiteData());
+		*/
+	}
+	
+	@FXML
+	public void ajouterVilleGridPane(ObservableList<Site> sites)
+	{
+		Label nomVille;
+		ImageView logoVille;
+		for(int i=0;i<this.getNbSite()/4;i++)
+		{
+			nomVille=new Label();
+			logoVille=new ImageView();
+			//On affecte aux Node les objets.
+			nomVille.setText(sites.get(i).getNomSte());
+			logoVille.setImage(new Image(sites.get(i).getCheminImage()));
+			//On positionne les Node
+			for (int j=0;j<4;j++)
+			{
+				gp_site.add(nomVille,j,i);
+				gp_site.add(logoVille,j,i);
+			}	
+		}
+	}
+	
+	public void setMainApp(MainApp mainApp)
+	{
+		this.mainApp=mainApp;
 	}
 
 }
