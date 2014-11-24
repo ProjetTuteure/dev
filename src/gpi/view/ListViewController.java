e714dc737526dd8359297beb2179337a342facf1
JulienPlas <Julien.plas19@live.fr>
2014-11-24 12:24:35 +0100

    controller

--- src/gpi/view/ListViewController.java
+++ src/gpi/view/ListViewController.java
@@ -1,4 +1,4 @@
-package gpi.metier.controller;
+package gpi.view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
--- src/gpi/view/RechercheAvanceController.java
+++ src/gpi/view/RechercheAvanceController.java
@@ -1,4 +1,4 @@
-package gpi.metier.controller;
+package gpi.view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
