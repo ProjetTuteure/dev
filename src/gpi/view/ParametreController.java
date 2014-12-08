af7d7c57742c80ea6742e9fdbb0b86af3d87d6e7
zkuhh <keket87@hotmail.fr>
2014-12-08 18:58:29 +0100

    changement .fxml et debut .java

--- src/gpi/view/ParametreController.java
+++ src/gpi/view/ParametreController.java
@@ -1,7 +1,158 @@
 package gpi.view;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
 
 public class ParametreController {
 
+	@FXML
+	private void addEtat(ActionEvent event) {
+	 
+	}
+	@FXML
+	private void modEtat(ActionEvent event) {
+	
+	}
+	@FXML
+	private void suppEtat(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addFabr(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modFabr(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppFabr(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addFact(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modFact(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppFact(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addLog(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modLog(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppLog(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addMaint(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modMaint(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppMaint(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addMat(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modMat(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppMat(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addPrest(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modPrest(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppPrest(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addRev(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modRev(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppRev(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addSite(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modSite(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppSite(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addType(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modType(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppType(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addUser(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modUser(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppUser(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void addUse(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void modUse(ActionEvent event) {
+	  
+	}
+	@FXML
+	private void suppUse(ActionEvent event) {
+	  
+	}
+	
+	
+	
 }
