532dd00d1f40944e2e3af33332c1c54073ba612a
JulienPlas <Julien.plas19@live.fr>
2014-12-13 22:36:56 +0100

    Adaptation AvanceController avec Donne

--- src/gpi/view/AvanceController.java
+++ src/gpi/view/AvanceController.java
@@ -1,6 +1,9 @@
 package gpi.view;
 
 import gpi.MainApp;
+import gpi.bd.Donnee;
+import gpi.metier.Site;
+import gpi.metier.Type;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,6 +12,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -22,25 +27,40 @@ public class AvanceController implements Initializable {
     @FXML
     private ComboBox<String> comboboxTypeAvanceOverview;
 
-    ObservableList<String> list1 = FXCollections.observableArrayList("Agen",
-            "Bordeaux", "Chateroux", "Guéret", "Limoges", "Montluçon",
-            "Saint Agan", "Saint Junien", "Tout");
+    private Donnee donnee=new Donnee();
+
+    ObservableList<Site> list1 = donnee.getSiteData();
     ObservableList<String> list2 = FXCollections.observableArrayList(
             "moins d'un ans", "moins de deux ans", "moins de trois ans",
             "moins de quattre ans", "moins de cinq ans", "moins de six ans",
             "moins de sept ans", "plus de sept ans");
-    ObservableList<String> list3 = FXCollections.observableArrayList(
-            "Ordinateur", "Switch", "Routeur", "Clé 3G", "Tout");
+    ObservableList<Type> list3 = donnee.getTypeData();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        comboboxSiteAvanceOverview.setItems(list1);
+        comboboxSiteAvanceOverview.setItems(getNomSite(list1));
         comboboxAncienneteAvanceOverview.setItems(list2);
-        comboboxTypeAvanceOverview.setItems(list3);
+        comboboxTypeAvanceOverview.setItems(getNomType(list3));
     }
 
     @FXML
     private void goToScreen2(ActionEvent event) {
         MainApp.changerTab("ResultatAvance");
+    }
+
+    public ObservableList<String> getNomSite(List<Site> sites){
+        ObservableList<String> list=FXCollections.observableArrayList();
+        for(Site site:sites){
+            list.add(site.getNomSte());
+        }
+        return list;
+    }
+
+    public ObservableList<String> getNomType(List<Type> types){
+        ObservableList<String> list=FXCollections.observableArrayList();
+        for(Type type:types){
+            list.add(type.getNom().getValue());
+        }
+        return list;
     }
 }
