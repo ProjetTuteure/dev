e87b269e330a59463c627b2c1bee984eff4264c8
Limmilkki <totor-du-40@hotmail.fr>
2014-12-13 18:23:36 +0100

    Solve conflict Données

--- src/gpi/bd/Donnee.java
+++ src/gpi/bd/Donnee.java
@@ -86,6 +86,7 @@ public class Donnee {
         typeData.add(new Type("PC"," "));
         typeData.add(new Type("Routeur"," "));
         typeData.add(new Type("Switch"," "));
+        typeData.add(new Type("Clé 3G"," "));
     }
 
     private void remplireSiteData(){
