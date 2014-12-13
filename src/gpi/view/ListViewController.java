31571b076d86ecc6dd52a4a8dd86e8ae68791489
JulienPlas <Julien.plas19@live.fr>
2014-12-13 10:57:45 +0100

    ListViewController -> DetailMachine

--- src/gpi/view/ListViewController.java
+++ src/gpi/view/ListViewController.java
@@ -1,25 +1,42 @@
 package gpi.view;
 
 import gpi.MainApp;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ListViewController implements Initializable {
+
+	private boolean test=false;
 
 	@FXML
 	private ListView<String> listMateriel;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		listMateriel.getItems().add("Materiel 1");
+		listMateriel.getItems().add("Materiel 1 0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
 		listMateriel.getItems().add("Materiel 2");
 		listMateriel.getItems().add("Materiel 3");
 		listMateriel.getItems().add("Materiel 4");
+
+		listMateriel.getSelectionModel().selectedItemProperty().addListener(
+				new ChangeListener<String>() {
+					public void changed(ObservableValue<? extends String> ov, String old_val, String new_val) {
+						if(test){
+							MainApp.changerTab("DetailMachine", 3);
+						}else{
+							test=true;
+						}
+
+					}
+				});
 	}
 
 	@FXML
@@ -28,9 +45,7 @@ public class ListViewController implements Initializable {
 	}
 
 	@FXML
-	private void goToScreen3(ActionEvent event) {
-		System.out.println(1);
-		MainApp.changerTab("Avance", 3);
-		System.out.println(2);
+	private void goToScreen3(MouseEvent event) {
+
 	}
 }
