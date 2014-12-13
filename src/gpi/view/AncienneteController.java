13e296985e4430429d6d61f5c251b77d69b6d94e
Limmilkki <totor-du-40@hotmail.fr>
2014-12-13 20:26:20 +0100

    Restriction combobox

--- src/gpi/view/AncienneteController.java
+++ src/gpi/view/AncienneteController.java
@@ -62,40 +62,91 @@ public class AncienneteController implements Initializable {
 		ObservableList<Site> site=this.mainApp.donnee.getSiteData();
 		ObservableList<Type> type=this.mainApp.donnee.getTypeData();
 		this.addDonneeTableView(materiel,site,type);
+		
+		comboboxSiteAncienneteOverview.setOnAction((event) -> {
+			actionOnCombo(materiel);
+		});
+		
+		comboboxTypeAncienneteOverview.setOnAction((event) -> {
+			actionOnCombo(materiel);
+		});
+	}
+	
+	public void actionOnCombo(ObservableList<Materiel> materiel){
+		String selectedSite="";
+		String selectedType="";
+		selectedSite = comboboxSiteAncienneteOverview.getSelectionModel().getSelectedItem();
+	    selectedType = comboboxTypeAncienneteOverview.getSelectionModel().getSelectedItem();
+		if(selectedSite==null){
+			selectedSite="Tous";
+		}
+		if(selectedType==null){
+			selectedType="Tous";
+		}
+	    if(selectedSite!=null || selectedType!=null){
+	    	addDonneeRestrictTableView(materiel,selectedSite,selectedType);
+	    }
 	}
 	
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
 	
+	public void setItemsTableMateriel(ObservableList<Materiel> materiel){
+		
+		materielTable.setItems(materiel);
+		nomMateriel.setCellValueFactory(cellData -> cellData.getValue().getNom());
+		dateAchatMateriel.setCellValueFactory(cellData -> cellData.getValue().getFacture().getDateFac());
+		etatMateriel.setCellValueFactory(cellData -> cellData.getValue().getEtatStringProperty());
+		finGarantieMateriel.setCellValueFactory(cellData -> cellData.getValue().getDateExpirationGarantie());
+		revendeurMateriel.setCellValueFactory(cellData -> cellData.getValue().getFacture().getRevendeur().getNomRev());
+		fabricantMateriel.setCellValueFactory(cellData -> cellData.getValue().getFabricant().getNomFab());
+		siteMateriel.setCellValueFactory(cellData -> cellData.getValue().getSite().getNomSteProperty());	
+		
+	}
+	
 	public void addDonneeTableView(ObservableList<Materiel> materiel,ObservableList<Site> site,ObservableList<Type> type){
 		ObservableList<String> listSite = FXCollections.observableArrayList();
 		ObservableList<String> listType = FXCollections.observableArrayList();
 		
-		materielTable.setItems(materiel);
-		nomMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getNom());
-		dateAchatMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getFacture().getDateFac());
-		etatMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getEtatStringProperty());
-		finGarantieMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getDateExpirationGarantie());
-		revendeurMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getFacture().getRevendeur().getNomRev());
-		fabricantMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getFabricant().getNomFab());
-		siteMateriel.setCellValueFactory(cellData -> cellData.getValue()
-				.getSite().getNomSteProperty());
+		setItemsTableMateriel(materiel);
 		
 		for(Site s : site){
 			listSite.add(s.getNomSte());
 		}
+		listSite.add("Tous");
 		for(Type t : type){
 			listType.add(t.getNomString());
 		}
+		listType.add("Tous");
 		
 		comboboxSiteAncienneteOverview.setItems(listSite);
 		comboboxTypeAncienneteOverview.setItems(listType);
 	}
+	
+	
+	public void addDonneeRestrictTableView(ObservableList<Materiel> materiel,String selectedSite, String selectedType){
+		ObservableList<Materiel> restrictedMateriel = FXCollections.observableArrayList();
+		boolean isOk;
+		for(Materiel m : materiel){
+			isOk=true;
+			System.out.println(selectedSite);
+			System.out.println(selectedType);
+			if(selectedSite!="Tous"){
+				if(m.getSiteString()!=selectedSite){
+					isOk=false;
+				}
+			}
+			if(selectedType!="Tous"){
+				if(m.getType().getNomString()!=selectedType){
+					isOk=false;
+				}
+			}
+			if(isOk){
+				restrictedMateriel.add(m);
+			}
+			setItemsTableMateriel(restrictedMateriel);
+		}
+	}
+	
 }
