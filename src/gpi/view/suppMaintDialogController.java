a468ca88d850b4e7e4b25eca6d4b5e85d775f9db
zkuhh <keket87@hotmail.fr>
2014-12-14 19:11:13 +0100

    suppr maintenance : :-1: probleme nb fois date
    Lorsque je selectionne l'objet, j'ai autant de fois le nombre de date
    que le nombre de maintenance faire.
    Il y a un total de 4 maintenances dans donnée, donc j'ai 4x la même date

--- src/gpi/view/suppMaintDialogController.java
+++ src/gpi/view/suppMaintDialogController.java
@@ -1,7 +1,12 @@
 package gpi.view;
 
+import gpi.bd.Donnee;
+import gpi.metier.Logiciel;
+import gpi.metier.Maintenance;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 /**
@@ -15,8 +20,25 @@ public class suppMaintDialogController {
 	private boolean okClicked = false;
 
 	@FXML
+	private ComboBox<String> comboboxobj;
+	@FXML
+	private ComboBox<String> comboboxdate;
+
+	private Donnee donnee = new Donnee();
+
+	private ObservableList<String> listobj;
+	private ObservableList<String> listdate;
+
+	@FXML
 	private void initialize() {
+		listobj = FXCollections.observableArrayList();
+
+		for (Maintenance m : donnee.getMaintenanceData()) {
+			listobj.add(m.getObjet());
+		}
+		comboboxobj.setItems(listobj);
 	}
+
 
 	public void setDialogStage(Stage dialogStage) {
 		this.dialogStage = dialogStage;
@@ -36,6 +58,17 @@ public class suppMaintDialogController {
 	@FXML
 	private void handleCancel() {
 		dialogStage.close();
+	}
+	
+	@FXML
+	private void handlechange() {
+		Maintenance selected = donnee.getMaintenance(comboboxobj.getValue());
+
+		listdate = FXCollections.observableArrayList();
+		for (Maintenance m : donnee.getMaintenanceData()) {
+			listdate.add(selected.getDateMaint());
+		}
+		comboboxdate.setItems(listdate);
 	}
 
 }
