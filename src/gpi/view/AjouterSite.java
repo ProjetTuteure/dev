32e4e03cf2fce158c5e35a5d3a1532744c143210
Limmilkki <totor-du-40@hotmail.fr>
2015-01-13 14:49:48 +0100

    Rennamed ajouter 2/2

---
+++ src/gpi/view/AjouterSite.java
@@ -0,0 +1,113 @@
+package gpi.view;
+
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ * Created by Kevin
+ */
+
+public class AjouterSite {
+	@FXML
+	private TextField NameSiteField;
+	@FXML
+	private Stage dialogStage;
+	// @FXML
+	// private Site site;
+	@FXML
+	private boolean okClicked = false;
+
+	/**
+	 * Initialise les données
+	 */
+	@FXML
+	private void initialize() {
+
+	}
+
+	/**
+	 * Cette methode permet de mettre en fenetre active le popup
+	 * 
+	 * @param dialogStage
+	 *            la fenetre active
+	 */
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+
+	// public void setPerson(Site site) {
+	// this.site = site;
+	//
+	// NameSiteField.setText(site.getNomSte());
+	//
+	// }
+
+	/**
+	 * Cette methode permet de savoir si le bouton AJOUTER est clique ou pas
+	 * 
+	 * @return vrai si le bouton AJOUTER est clique, faux sinon
+	 */
+	public boolean isOkClicked() {
+		return okClicked;
+	}
+
+	/**
+	 * Cette procedure permet de fermer la fenetre, lorsque le bouton AJOUTER
+	 * est clique
+	 */
+	@FXML
+	private void handleOk() {
+		// if (isInputValid()) {
+		// site.setNomSte(NameSiteField.getText());
+
+		okClicked = true;
+		dialogStage.close();
+		// }
+	}
+
+	/**
+	 * Cette procedure permet de fermer la fenetre, lorsque le bouton ANNULER
+	 * est clique
+	 */
+	@FXML
+	private void handleCancel() {
+		dialogStage.close();
+	}
+
+	// private boolean isInputValid() {
+	// String errorMessage = "";
+	//
+	// if (NameSiteField.getText() == null
+	// || NameSiteField.getText().length() == 0) {
+	// errorMessage += "Nom de site invalide\n";
+	// }
+	//
+	// if (errorMessage.length() == 0) {
+	// return true;
+	// } else {
+	// // Show the error message
+	// Dialogs.showErrorDialog(dialogStage, errorMessage,
+	// "Veuillez corriger le champ", "Champ invalide");
+	// return false;
+	// }
+	// }
+
+	@FXML
+	private void handleChoose(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open File");
+		File file = fileChooser.showOpenDialog(null); // you could pass a stage
+														// reference here if you
+														// wanted.
+
+		if (file != null) {
+		}// do something interesting with the file.
+
+	}
+
+}
