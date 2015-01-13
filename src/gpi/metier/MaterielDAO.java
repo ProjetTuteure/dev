08c09187d9d92f5aa35cedfad365fa3959bec250
cedricCaffy <cedric.caffy@hotmail.fr>
2015-01-13 16:47:15 +0100

    insererMateriel

--- src/gpi/metier/MaterielDAO.java
+++ src/gpi/metier/MaterielDAO.java
@@ -13,8 +13,23 @@ public class MaterielDAO {
 		this.connexion=connexion;
 	}
 	
+	/**
+	 * Permet d'ajouter un matériel dans la base de données
+	 * @param materiel le matériel à ajouter dans la base de données
+	 */
 	public void ajouterMateriel(Materiel materiel)
 	{
-		String requete="INSERT INTO MATERIEL (numImmobMateriel, VALUES("+materiel.getNumImmobMateriel()+","
+		String requete="INSERT INTO MATERIEL (numImmobMateriel,nomMateriel,dateExpirationGarantieMateriel,"
+				+ "repertoireDriver,modeleMateriel,etat,idFacture,idFabricant,idSite,nomType) "
+				+ "VALUES("+materiel.getNumImmobMateriel()+","+materiel.getNomMateriel()+","
+				+ materiel.getDateExpirationGarantieMaterielStringProperty().getValue()+","
+				+ materiel.getRepertoireDriverMateriel().getValue()+","
+				+ materiel.getModeleMateriel()+","
+				+ materiel.getEtatMaterielString()+","
+				+ materiel.getFactureMateriel().getNumFacture()+","
+				+ materiel.getFabricantMateriel().getIdFabricant()+","
+				+ materiel.getSiteMateriel().getIdSite()+","
+				+ materiel.getTypeMateriel().getNomTypeString()
+				+")";
 	}
 }
