2b188472f188095cea9cf2291e589c3d3780573a
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-01-14 11:38:53 +0100

    UtiliserDAO

---
+++ src/gpi/metier/UtiliseDAO.java
@@ -0,0 +1,62 @@
+package gpi.metier;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import utils.MaConnexion;
+
+public class UtiliseDAO {
+	
+	public UtiliseDAO() {
+	}
+	
+	public int ajouterUtilise(Utilise utilise){
+		Connection connexion = MaConnexion.getInstance().getConnexion();
+		int resultat;
+		try {
+			connexion = MaConnexion.getInstance().getConnexion();
+			PreparedStatement prep = connexion
+					.prepareStatement("INSERT INTO SITE (idUtilisateur, idMateriel)  VALUES (?,?);");
+
+			prep.setInt(1, utilise.getUtilisateurUtilise().getIdUtilisateur().getValue());
+			prep.setInt(2, utilise.getMaterielUtilise().getIdMateriel().getValue());
+			resultat = prep.executeUpdate();
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connexion.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
+	}
+	
+	public int supprimerUtlise(Utilise utilise){
+		Connection connexion = MaConnexion.getInstance().getConnexion();
+		int resultat;
+		try {
+			connexion = MaConnexion.getInstance().getConnexion();
+			PreparedStatement prep = connexion
+					.prepareStatement("DELETE FROM UTILISE WHERE idUtilisateur=? And idMateriel=?;");
+			prep.setInt(1, utilise.getUtilisateurUtilise().getIdUtilisateur().getValue());
+			prep.setInt(2, utilise.getMaterielUtilise().getIdMateriel().getValue());
+			resultat = prep.executeUpdate();
+			return resultat;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connexion.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return 0;
+	}
+	
+	
+}
