f63af7c8be05e2a2c186f944a11681e12260cdc9
zkuhh <keket87@hotmail.fr>
2015-01-14 11:43:41 +0100

    :hamburger: :ice_cream:

---
+++ src/gpi/metier/UtilisateurDAO.java
@@ -0,0 +1,45 @@
+package gpi.metier;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import utils.MaConnexion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//public class UtilisateurDAO {
+//
+//	public UtilisateurDAO(){
+//		
+//		public int ajouterUtilisateur(Utilisateur ut){
+//			Connection connection=null;
+//			int resultat;
+//			try{
+//				connection=MaConnexion.getInstance().getConnexion();
+//				PreparedStatement prep=connection.prepareStatement("INSERT INTO UTILISATEUR(nomUtilisateur,prenomUtilisateur,telUtilisateur) VALUES (?,?,?);");
+//				
+//				prep.setString(1, ut.getNomUtilisateur());
+//				
+//			}
+//			
+//		}
+		
+		
+		
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}
+
+
+
+
