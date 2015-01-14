243580bbc8564b6594c0f1891794d7d886ed5bd0
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-01-14 08:30:48 +0100

    Lib

---
+++ src/gpi/metier/ComposantDAO.java
@@ -0,0 +1,36 @@
+package gpi.metier;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import utils.MaConnexion;
+
+public class ComposantDAO {
+
+	public ComposantDAO(){}
+	
+	public int ajouterComposant(Composant composant){
+		Connection connexion = MaConnexion.getInstance().getConnexion();
+		Connection connection;
+		int resultat;
+		try{
+			connection=MaConnexion.getInstance().getConnexion();
+			PreparedStatement prep = connection.prepareStatement("INSERT INTO SITE VALUES (?,?,?);");
+			
+			prep.setInt(1, site.getIdSite());
+			prep.setString(2, site.getNomSiteString());
+			prep.setString(3, site.getCheminImageSiteProperty().toString());
+			
+			resultat=prep.executeUpdate();
+			return resultat;
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			connection.close();
+		}
+		
+		
+		return 1;
+	}
+}
