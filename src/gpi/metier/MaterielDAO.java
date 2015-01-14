8c2ca8a1794ce23effecd221d82289c1d872daeb
cedricCaffy <cedric.caffy@hotmail.fr>
2015-01-14 11:01:32 +0100

    materielDAO

--- src/gpi/metier/MaterielDAO.java
+++ src/gpi/metier/MaterielDAO.java
@@ -52,17 +52,50 @@ public class MaterielDAO {
 		}
 	}
 	
+	/**
+	 * Permet de modifier un materiel
+	 * @param materiel le materiel à modifier
+	 */
 	public void modifierMateriel(Materiel materiel)
 	{
 		Connection connexion=MaConnexion.getInstance().getConnexion();
 		try {
-			PreparedStatement ps=connexion.prepareStatement("UPDATE MATERIEL SET numImmobMateriel,nomMateriel,dateExpirationGarantieMateriel,"
-					+ "repertoireDrivers,modeleMateriel,etat,idFacture,idFabricant,idSite,nomType  "
-					+ "VALUES(?,?,?,?,?,?,?,?,?,?) WHERE idMateriel=?");
+			PreparedStatement ps=connexion.prepareStatement("UPDATE MATERIEL SET numImmobMateriel=?,nomMateriel=?,dateExpirationGarantieMateriel=?,"
+					+ "repertoireDrivers=?,modeleMateriel=?,etat=?,idFacture=?,idFabricant=?,idSite=?,nomType=?  "
+					+ "WHERE idMateriel=?");
+			ps.setString(1,materiel.getNumImmobMateriel().getValue());
+			ps.setString(2,materiel.getNomMateriel().getValue());
+			ps.setString(3,materiel.getDateExpirationGarantie().toString());
+			ps.setString(4,materiel.getRepertoireDriverMateriel().getValue());
+			ps.setString(5,materiel.getModeleMateriel());
+			ps.setString(6,materiel.getEtatMateriel().toString());
+			ps.setString(7,materiel.getFactureMateriel().getNumFacture());
+			ps.setInt(8,materiel.getFabricantMateriel().getIdFabricant().getValue());
+			ps.setInt(9,materiel.getSiteMateriel().getIdSiteProperty().getValue());
+			ps.setString(10,materiel.getTypeMateriel().getNomType().getValue());
+			ps.setInt(11, materiel.getIdMateriel().getValue());
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally
+		{
+			try {
+				connexion.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Permet de supprimer un materiel
+	 * @param materiel le materiel à supprimer
+	 */
+	public void supprimerMateriel(Materiel materiel)
+	{
+		
 	}
 }
