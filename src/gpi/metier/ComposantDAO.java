56a03d0063d080f2d062508136ad63bc13fb403c
zkuhh <keket87@hotmail.fr>
2015-01-15 11:08:41 +0100

    composantDAO :santa: :

--- src/gpi/metier/ComposantDAO.java
+++ src/gpi/metier/ComposantDAO.java
@@ -1,80 +1,71 @@
 package gpi.metier;
 
 import gpi.exception.ConnexionBDException;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import javafx.beans.property.SimpleIntegerProperty;
 import utils.MaConnexion;
 
 public class ComposantDAO {
 
+	FabricantDAO fabricantDAO = new FabricantDAO();
+
 	public ComposantDAO() {
 	}
 
-	public int ajouterComposant(Composant composant) throws ConnexionBDException{
+	public void ajouterComposant(Composant composant)
+			throws ConnexionBDException {
 		Connection connexion = MaConnexion.getInstance().getConnexion();
-		int resultat;
 		try {
-			connexion = MaConnexion.getInstance().getConnexion();
 			PreparedStatement prep = connexion
-					.prepareStatement("INSERT INTO SITE (nomComposant, caracteristiqueComposant, idFabricant)  VALUES (?,?,?);");
+					.prepareStatement("INSERT INTO COMPOSANT (nomComposant, caracteristiqueComposant, idFabricant) "
+							+ "VALUES (?,?,?);");
 
 			prep.setString(1, composant.getNomComposant());
 			prep.setString(2, composant.getcaracteristiqueComposant());
 			prep.setInt(3, composant.getFabricantComposant().getIdFabricant()
 					.getValue());
-
-			resultat = prep.executeUpdate();
-			return resultat;
+			prep.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				connexion.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
 			}
 		}
-		return 0;
 	}
 
-	public int suprimerComposant(Composant composant) throws ConnexionBDException{
-		
+	public void suprimerComposant(Composant composant)
+			throws ConnexionBDException {
+
 		Connection connexion = MaConnexion.getInstance().getConnexion();
-		int resultat;
 		try {
-			connexion = MaConnexion.getInstance().getConnexion();
 			PreparedStatement prep = connexion
 					.prepareStatement("DELETE FROM COMPOSANT WHERE idComposant = ?;");
 
 			prep.setInt(1, composant.getIdComposant());
-			resultat = prep.executeUpdate();
-			return resultat;
+			prep.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				connexion.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
 			}
 		}
-		return 0;
 	}
 
-	public int modifierComposant(Composant composant) throws ConnexionBDException{
+	public void modifierComposant(Composant composant)
+			throws ConnexionBDException {
 		Connection connexion = MaConnexion.getInstance().getConnexion();
-		int resultat;
 		try {
-			connexion = MaConnexion.getInstance().getConnexion();
 			PreparedStatement prep = connexion
 					.prepareStatement("UPDATE COMPOSANT SET nomComposant=?, caracteristiqueComposant=?, idFabricant=? WHERE idComposant=?");
 
@@ -83,79 +74,71 @@ public class ComposantDAO {
 			prep.setInt(3, composant.getFabricantComposant().getIdFabricant()
 					.getValue());
 			prep.setInt(4, composant.getIdComposant());
-			resultat = prep.executeUpdate();
-			return resultat;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				connexion.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
 			}
 		}
-		return 0;
-
 	}
 
-	public Composant recupererComposantParId(int idComposant) throws ConnexionBDException{
-		FabricantDAO fabricantDAO = new FabricantDAO();
+	public Composant recupererComposantParId(int idComposant)
+			throws ConnexionBDException {
 		Connection connexion = MaConnexion.getInstance().getConnexion();
-		ResultSet resultat;
-		ResultSetMetaData resultMeta;
+		FabricantDAO fabricantDAO = new FabricantDAO();
+		Composant composantARetourner = null;
 		try {
 			PreparedStatement prep = connexion
 					.prepareStatement("SELECT * from COMPOSANT WHERE idcomposant=?");
 
 			prep.setInt(1, idComposant);
-			resultat = prep.executeQuery();
-			resultMeta = resultat.getMetaData();
-			String nomComposant = resultat.getString(2);
-			String caracteristiqueComposant = resultat.getString(3);
-			int idFabricant = resultat.getInt(4);
-			return new Composant(new SimpleIntegerProperty(idComposant),
-					nomComposant, caracteristiqueComposant,
-					fabricantDAO.recupererFabricantParId(idFabricant));
+			ResultSet resultat = prep.executeQuery();
+			while (resultat.next()) {
+				composantARetourner = new Composant(new SimpleIntegerProperty(
+						resultat.getInt("idComposant")),
+						resultat.getString("nomComposant"),
+						resultat.getString("caracteristiqueComposant"),
+						fabricantDAO.recupererFabricantParId(resultat
+								.getInt("idFabricant")));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				connexion.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
 			}
 		}
-		return null;
+		return composantARetourner;
 	}
 
-	public List recupererAllComposant() throws ConnexionBDException{
-		List<Composant> listComposant = new ArrayList<Composant>();
-		FabricantDAO fabricantDAO = new FabricantDAO();
+	public List<Composant> recupererAllComposant() throws ConnexionBDException {
 		Connection connexion = MaConnexion.getInstance().getConnexion();
-		ResultSet resultat;
-		ResultSetMetaData resultMeta;
+		List<Composant> listComposant = new ArrayList<Composant>();
+
 		try {
 			PreparedStatement prep = connexion
 					.prepareStatement("SELECT * from COMPOSANT");
-			resultat = prep.executeQuery();
-			resultMeta = resultat.getMetaData();
+			ResultSet resultat = prep.executeQuery();
 			while (resultat.next()) {
-				int idComposant = resultat.getInt(1);
-				String nomComposant = resultat.getString(2);
-				String caracteristiqueComposant = resultat.getString(3);
-				int idFabricant = resultat.getInt(4);
 				listComposant.add(new Composant(new SimpleIntegerProperty(
-						idComposant), nomComposant, caracteristiqueComposant,
-						fabricantDAO.recupererFabricantParId(idFabricant)));
+						resultat.getInt("idComposant")), resultat
+						.getString("nomComposant"), resultat
+						.getString("caracteristiqueComposant"),
+						fabricantDAO.recupererFabricantParId(resultat
+								.getInt("idFabricant"))));
 			}
-			return listComposant;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				connexion.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
 			}
 		}
 		return listComposant;
