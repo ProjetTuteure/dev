46e6bee4b58b467e4d9ac616bfb34bbc4be468d5
cedricCaffy <cedric.caffy@hotmail.fr>
2015-01-15 11:28:40 +0100

    ajouterMaintenance

--- src/gpi/metier/MaintenanceDAO.java
+++ src/gpi/metier/MaintenanceDAO.java
@@ -25,7 +25,6 @@ public class MaintenanceDAO {
 			ps.setFloat(4,maintenance.getCoutMaintenance());
 			ps.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		finally
