48def565db4d010bcff29f1187a3a5d9fc984dcc
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-01-15 08:20:03 +0100

    Trigger sql

--- BD/base.sql
+++ BD/base.sql
@@ -157,3 +157,15 @@ AS BEGIN
 	delete from facture where idrevendeur = @idrevendeur
 END
 
+CREATE TRIGGER SuppressionMateriel on MATERIEL
+AFTER DELETE
+AS BEGIN 
+	declare @idMateriel int;
+	SELECT @idMateriel=idMateriel FROM deleted
+	DELETE FROM UTILISE WHERE idMateriel=@idMateriel
+	DELETE FROM ESTMAINTENU WHERE idMateriel=@idMateriel
+	DELETE FROM COMPOSE WHERE idMateriel=@idMateriel
+END
+
+
+
