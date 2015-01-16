8f4195eb14fe12e02e012c60c84f894ec64a4654
cedricCaffy <cedric.caffy@hotmail.fr>
2015-01-16 08:05:59 +0100

    estMaintenu class

--- src/gpi/metier/estMaintenu.java
+++ src/gpi/metier/estMaintenu.java
@@ -1,38 +1,37 @@
 package gpi.metier;
-
-/**
- * Created by Julien on 15/01/2015.
- */
+/*
+* Created by Julien on 15/01/2015.
+*/
 public class EstMaintenu {
-    private Materiel materielEstMaintenu;
-    private Maintenance maintenanceEstMaintenu;
+   private Materiel materielEstMaintenu;
+   private Maintenance maintenanceEstMaintenu;
 
-    public EstMaintenu(Maintenance maintenanceEstMaintenu,Materiel materielEstMaintenu) {
-        this.materielEstMaintenu = materielEstMaintenu;
-        this.maintenanceEstMaintenu = maintenanceEstMaintenu;
-    }
+   public EstMaintenu(Maintenance maintenanceEstMaintenu,Materiel materielEstMaintenu) {
+       this.materielEstMaintenu = materielEstMaintenu;
+       this.maintenanceEstMaintenu = maintenanceEstMaintenu;
+   }
 
-    public Materiel getMaterielEstMaintenu() {
-        return this.materielEstMaintenu;
-    }
+   public Materiel getMaterielEstMaintenu() {
+       return this.materielEstMaintenu;
+   }
 
-    public int getIdMaterielEstMaintenu() {
-        return this.materielEstMaintenu.getIdMateriel().getValue();
-    }
+   public int getIdMaterielEstMaintenu() {
+       return this.materielEstMaintenu.getIdMateriel().getValue();
+   }
 
-    public void setMaterielEstMaintenu(Materiel materielEstMaintenu) {
-        this.materielEstMaintenu = materielEstMaintenu;
-    }
+   public void setMaterielEstMaintenu(Materiel materielEstMaintenu) {
+       this.materielEstMaintenu = materielEstMaintenu;
+   }
 
-    public Maintenance getMaintenanceEstMaintenu() {
-        return this.maintenanceEstMaintenu;
-    }
+   public Maintenance getMaintenanceEstMaintenu() {
+       return this.maintenanceEstMaintenu;
+   }
 
-    public int getIdMaintenanceEstMaintenu() {
-        return this.maintenanceEstMaintenu.getIdMaintenance().getValue();
-    }
+   public int getIdMaintenanceEstMaintenu() {
+       return this.maintenanceEstMaintenu.getIdMaintenance().getValue();
+   }
 
-    public void setMaintenanceEstMaintenu(Maintenance maintenanceEstMaintenu) {
-        this.maintenanceEstMaintenu = maintenanceEstMaintenu;
-    }
+   public void setMaintenanceEstMaintenu(Maintenance maintenanceEstMaintenu) {
+       this.maintenanceEstMaintenu = maintenanceEstMaintenu;
+   }
 }
