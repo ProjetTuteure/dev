76200155ea4ea18b6d5f4f96805a249c295a46d2
cedricCaffy <cedric.caffy@hotmail.fr>
2015-02-02 17:15:37 +0100

    Trigger supprimer site

--- BD/TriggerSuppressionSite.sql
+++ BD/TriggerSuppressionSite.sql
@@ -2,7 +2,15 @@ CREATE TRIGGER supprimerSite ON SITE
 INSTEAD OF DELETE AS
 BEGIN
 	DECLARE @idSite int
+	DECLARE @idMateriel INT
 	SELECT @idSite=idSite FROM DELETED
-	DELETE FROM MATERIEL WHERE idSite=@idSite
+	DECLARE CursorMateriel CURSOR FOR SELECT idMateriel FROM MATERIEL WHERE idSite=@idSite
+	OPEN CursorMateriel
+	FETCH CursorMateriel INTO @idMateriel
+	WHILE @@FETCH_STATUS=0
+	BEGIN
+		DELETE FROM MATERIEL WHERE idMateriel=@idMateriel
+		FETCH CursorMateriel INTO @idMateriel
+	END
 	DELETE FROM SITE WHERE idSite=@idSite
 END
