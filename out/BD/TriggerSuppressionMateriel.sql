aabce58e20a2c2ee6e6f004ae01b91ba52947189
cedricCaffy <cedric.caffy@hotmail.fr>
2015-02-02 15:38:19 +0100

    Tripotasse

--- out/BD/TriggerSuppressionMateriel.sql
+++ out/BD/TriggerSuppressionMateriel.sql
@@ -2,10 +2,19 @@ CREATE TRIGGER SuppressionMateriel on MATERIEL
 INSTEAD OF DELETE
 AS BEGIN 
 	declare @idMateriel int;
-	SELECT @idMateriel=idMateriel FROM deleted
-	DELETE FROM UTILISE WHERE idMateriel=@idMateriel
-	DELETE FROM ESTMAINTENU WHERE idMateriel=@idMateriel
-	DELETE FROM COMPOSE WHERE idMateriel=@idMateriel
-	DELETE FROM ESTINSTALLE WHERE idMateriel=@idMateriel
-	DELETE FROM MATERIEL WHERE idMateriel=@idMateriel
+	DECLARE @rows int
+	DECLARE MyCursor CURSOR FOR SELECT idMateriel FROM DELETED
+	OPEN MyCursor
+	FETCH MyCursor INTO @idMateriel
+	WHILE @@FETCH_STATUS=0
+	BEGIN
+		DELETE FROM UTILISE WHERE idMateriel=@idMateriel
+		DELETE FROM ESTMAINTENU WHERE idMateriel=@idMateriel
+		DELETE FROM COMPOSE WHERE idMateriel=@idMateriel
+		DELETE FROM ESTINSTALLE WHERE idMateriel=@idMateriel
+		DELETE FROM MATERIEL WHERE idMateriel=@idMateriel
+		FETCH MyCursor INTO @idMateriel
+	END
+	CLOSE MyCursor
+	DEALLOCATE MyCursor
 END
